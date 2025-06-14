7e1afa79184c1590d7003c3e7b465b4aa5ac5968
LucasAiup <45949066@terciariourquiza.edu.ar>
2025-06-14 20:57:20 -0300

    Create consola_calculo_calorias_reposo.py

---
+++ consola_calculo_calorias_reposo.py
@@ -0,0 +1,7 @@
+import CalculadoraIMC
+a=input("edad")  
+b=input("peso")
+c=input("altura en metros")
+d=input("Sexo h / m")
+test= CalculadoraIMC.calculadora_IMC(a,0,b,c,d,0)
+print(round(test.calcular_calorias_en_reposo(), 2))
