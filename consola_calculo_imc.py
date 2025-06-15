5d9936eed8a3f39b8221fefd343301585aa5064c
LucasAiup <45949066@terciariourquiza.edu.ar>
2025-06-14 21:13:14 -0300

    Update consola_calculo_imc.py

--- consola_calculo_imc.py
+++ consola_calculo_imc.py
@@ -2,4 +2,4 @@ import CalculadoraIMC
 c=input("peso")
 d=input("altura en metros")
 test= CalculadoraIMC.calculadora_IMC(0,0,c,d,0)
-print(round(test.calcular_IMC(), 2))
+print(f"Resultado:{round(test.calcular_IMC(), 2)}")
